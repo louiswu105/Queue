4bdfa3b78d81a6181bb433d88b6c4b012e3e5115
Louis <louishouse105@gmail.com>
2017-05-10 17:22:09 +0800

    parallax scroll script

--- js/queue.js
+++ js/queue.js
@@ -28,4 +28,93 @@
         }
     })
 
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile|Edge/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        },
+        disableFixed: function() {
+            return (isMobile.Android());    
+        }
+    };
+
+    var enableParallax =
+    {
+        parallaxSelector: '.parallax .col-sm-5 img',
+        ratioDefault: 6,
+        
+        init: function() {
+            // enanble only if parallax selector exists
+            if (    !$(this.parallaxSelector).length) return false;
+            
+            // disable on devices with IEmobile
+            if (isMobile.Windows()) return false;
+            
+            var _self = this;
+            
+            this.setParallax();
+            
+            $(window).scroll(function(){
+                _self.setParallax();
+            });
+        },
+        
+        setParallax: function() {
+            var _self = this;
+            var scrollTop = $(window).scrollTop();
+            var scrollBottom = scrollTop + $(window).height();
+            
+            $(this.parallaxSelector).each(function() {
+                var elOffsetTop = $(this).offset().top;
+                var positionTopBase = scrollBottom - elOffsetTop;
+                
+                if (elOffsetTop <= $(window).height()) {
+                    positionTopBase = $(window).scrollTop();
+                }
+                
+                var ratio = _self.ratioDefault;
+                
+                if ($(this).data('parallax-ratio') > 0) {
+                    ratio = $(this).data('parallax-ratio');
+                }
+                
+                var positionTop = Math.round(positionTopBase / ratio);
+                
+                if (elOffsetTop > scrollBottom) {
+                    $(this).css({
+                        "-moz-transform": "",
+                        "-o-transform": "",
+                        "-ms-transform": "",
+                        "-webkit-transform": "",
+                        "transform": ""
+                    });
+    
+                } else {
+                    $(this).css({
+                        "-moz-transform": "translate3d(0px, " + positionTop + "px, 0px)",
+                        "-o-transform": "translate3d(0px, " + positionTop + "px, 0px)",
+                        "-ms-transform": "translate3d(0px, " + positionTop + "px, 0px)",
+                        "-webkit-transform": "translate3d(0px, " + positionTop + "px, 0px)",
+                        "transform": "translate3d(0px, " + positionTop + "px, 0px)"
+                    });
+                }
+            });
+        }
+    };
+
+    enableParallax.init();
 })(jQuery); // End of use strict
