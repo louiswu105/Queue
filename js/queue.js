cd1b5dc05fabcf5c5180779f8d688b5006190b2c
Louis <louishouse105@gmail.com>
2017-05-11 03:23:33 +0800

    test wave

--- js/queue.js
+++ js/queue.js
@@ -1,3 +1,36 @@
+$( document ).ready(function() {
+    anime({
+      targets: 'path',
+      strokeDashoffset: function(el) {
+        var pathLength = el.getTotalLength();
+        el.setAttribute('stroke-dasharray', pathLength);
+        return [-pathLength, 0];
+      },
+      stroke: {
+        value: function(el, i) {
+          return 'rgb(200,'+ i * 8 +',150)'; 
+        },
+        easing: 'linear',
+        duration: 2000,
+      },
+      strokeWidth: {
+        value: 6,
+        easing: 'linear',
+        delay: function(el, i) { 
+          return 1200 + (i * 40); 
+        },
+        duration: 800,
+      },
+      delay: function(el, i) { 
+        return i * 60; 
+      },
+      duration: 1200,
+      easing: 'easeOutExpo',
+      loop: true,
+      direction: 'alternate'
+    });
+});
+
 (function($) {
     "use strict"; // Start of use strict
 
